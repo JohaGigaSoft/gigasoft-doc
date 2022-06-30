7f7b34b475b804ee77d9238b28cd72a5b4f18ef5
JohaGigaSoft <joha.gigasoft@gmail.com>
2022-06-30 14:18:41 +0600

    modify the home file

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,7 @@ const Home=()=>{
         <h1 className='fs-1 bg-primary'>Home Page Now</h1>
         <p>this is a paragraph tag</p>
         <p>React component</p>
+        <h1>hello bangladesh</h1>
         
        </>
    
